da5d0755ba2a3dd801e08101ce3befe55a15f498
lan <1109373967@qq.com>
2020-11-20 20:48:05 +0800

    first

---
+++ VueStudy/helloword.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <div v-for="item in items" :key="item.id">
+            {{ item }}
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                key: value
+            }
+        },
+        methods: {
+            name() {
+                
+            }
+        },
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
